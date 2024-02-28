f65b9dfe75994eb510eac379559d839f1f5133a4
luanmoraes123 <luan.moraees21@gmail.com>
2024-02-28 20:18:57 -0300

    migrando para typescript

---
+++ src/config/knex.ts
@@ -0,0 +1,10 @@
+export default {
+  client: 'mysql2',
+  connection: {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '010203',
+    database: 'testednc',
+  },
+};
---
+++ src/module/aluno/aluno.model.ts
@@ -0,0 +1,17 @@
+import {KnexService} from '../../service/knex';
+import {Knex} from 'knex'
+
+export default class Aluno{
+   db: Knex
+   constructor(KnexService: KnexService){
+    this.db = KnexService.obterConexao();
+   }
+
+   getAll = () => {
+    return this.db('aluno').select();
+   }
+
+   store = (params: Aluno) => {
+    return this.db().insert(params);
+   }
+}
